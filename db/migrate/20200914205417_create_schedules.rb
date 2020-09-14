f8c3f1568924830357623c32b82437f25e570de8
Walter de Camargo Grangeiro <walter.camargo.gr@gmail.com>
2020-09-14 18:01:44 -0300

    Create a schedule table

---
+++ db/migrate/20200914205417_create_schedules.rb
@@ -0,0 +1,8 @@
+class CreateSchedules < ActiveRecord::Migration[6.0]
+  def change
+    create_table :schedules do |t|
+      t.datetime :started_at
+      t.datetime :finished_at
+    end
+  end
+end
